52dbd49038a3bc8a1afa3f4a8916520f46518a60
Bhawani Shanker <bhawani_s@trigent.com>
2014-02-13 13:10:53 +0530

    second commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+$WSDL_PATH = '';
+$soap_obj = new soapclient($WSDL_PATH); 
+
+$params = array();
+$response =  $soap_obj->WebServiceMethodNameWillComeHere($params);
+
+if (is_soap_fault($response)) {
+    return false;   
+}else{
+    return $response->WebServiceMethodNameWillComeHereReturn;
+}
+
+echo "end";
+?>
